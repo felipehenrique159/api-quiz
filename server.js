242ff2fa3d79beb810fe4759b88548fc96c081e7
felipehenrique159 <felipecod6@hotmail.com>
2023-01-26 00:09:30 -0300

    Instalado Cors

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const cors = require('cors')
+const routes = require('./src/routes')
+
 const app = express()
+
 app.listen(3000)
-app.use('/api', require('./src/routes'))
+
+app.use(cors())
+app.use('/api', routes)
